3fd333081edde294b0322f4fe41042b2fe68bb22
Hoang Long <tramlang31@gmail.com>
2023-02-20 16:13:18 +0700

    update

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const home = require("./routes/home");
 
 // Middlewares
 const app = express();
-require('./middleParty')(app)
+// require('./middleParty')(app)
+ // cors
+ const cors = require('cosrs')
+ app.use(cors({
+         origin: '*'
+ }))
+
+ // content-type access
+ app.use(express.json());
+
 
 // Routes
 // app.use("/home", home);
