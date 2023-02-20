6ad5e6d2ec7ecae086577360f6bea560c9c752da
Hoang Long <tramlang31@gmail.com>
2023-02-20 22:02:42 +0700

    update

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,12 +1,12 @@
 const authRouter = require('express').Router()
 
 const auth = require('../controllers/auth.controller')
-// const authMiddleware = require('../middlewares/auth.middleware')
-// const upload = require('../middlewares/uploadFile')
+const authMiddleware = require('../middlewares/auth.middleware')
+const upload = require('../middlewares/uploadFile')
 
 
-// authRouter.post('/signUp', authMiddleware.checkIsUserExist, auth.signUp);
-// authRouter.post('/signIn', authMiddleware.checkIsUserExist, auth.signIn);
+authRouter.post('/signUp', authMiddleware.checkIsUserExist, auth.signUp);
+authRouter.post('/signIn', authMiddleware.checkIsUserExist, auth.signIn);
 
 authRouter.get('/user/account/profile', auth.profile)
 authRouter.post('/user/account/profile/update', auth.updateProfile)
@@ -14,7 +14,7 @@ authRouter.get('/user/account/profile/address', auth.address)
 authRouter.post('/user/account/profile/address/add', auth.addAddress)
 authRouter.post('/user/account/profile/address/update', auth.updateAddress)
 
-// authRouter.post('/user/account/profile/avatar/update', upload.single('avatar'), auth.updateAvatar)
+authRouter.post('/user/account/profile/avatar/update', upload.single('avatar'), auth.updateAvatar)
 authRouter.get('/user/account/profile/avatar', auth.avatar)
 
 module.exports = authRouter;
